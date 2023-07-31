eb2bcd5aaffc1ce42adcc99d0df644c2fb111d85
wlstmd1004v <wlstmd1004v@gmail.com>
2023-07-31 14:02:58 +0900

    add

---
+++ client/js/main.js
@@ -0,0 +1,10 @@
+
+
+let swiper = new Swiper('.swiper',{
+    autoplay: false,
+    loop: true,
+    navigation : {
+        nextEl : '.swiper-button-next',
+        prevEl : '.swiper-button-prev',
+    },
+});
