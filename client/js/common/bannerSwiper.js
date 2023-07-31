8c93194a46092eaefc532ec4f7a21d0633ea1b85
wlstmd1004v <wlstmd1004v@gmail.com>
2023-08-01 04:47:59 +0900

    Refactor : merge 병합

--- client/js/common/bannerSwiper.js
+++ client/js/common/bannerSwiper.js
@@ -1,5 +1,5 @@
 //swiper 기능 구현
-export const swiper = new Swiper('#swiper', {
+const swiper = new Swiper('#swiper', {
   slidesPerView: 'auto',
   centeredSlides: true,
   spaceBetween: 0,
@@ -23,8 +23,28 @@ export const swiper = new Swiper('#swiper', {
   speed: 3.5, // 트랜지션 속도를 0ms로 설정
 });
 
-export let swiperOnly = new Swiper('.mySwiper', {
+let swiperOnly = new Swiper('.mySwiper', {
   slidesPerView: 6,
   // spaceBetween: 15,
   freeMode: true,
+});
+
+const firstSwiper = new Swiper('#first-swiper', {
+  slidesPerView: 6,
+  spaceBetween: 5,
+  // Navigation arrows
+  navigation: {
+    nextEl: '#swiper-button-next-first',
+    prevEl: '#swiper-button-prev-first',
+  },
+});
+
+const secondSwiper = new Swiper('#second-swiper', {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  // Navigation arrows
+  navigation: {
+    nextEl: '#swiper-button-next-second',
+    prevEl: '#swiper-button-prev-second',
+  },
 });
