9736134177b3459716d2969d9e791b2a447d26cc
chowonn <hcw12321@gmail.com>
2023-07-31 17:17:23 +0900

    Feat : swiper 기능 구현

---
+++ client/js/common/bannerSwiper.js
@@ -0,0 +1,26 @@
+//swiper 기능 구현
+export const swiper = new Swiper("#swiper", {
+ 
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: true, 
+  },
+  loop: true,
+  loopAdditionalSlides: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  fadeEffect: {
+    crossFade: true
+  },
+  speed: 3.5 // 트랜지션 속도를 0ms로 설정
+});
+
