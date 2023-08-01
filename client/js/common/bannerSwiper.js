dac3dfecd4613431880546067eed404c39df4ff2
wlstmd1004v <wlstmd1004v@gmail.com>
2023-08-01 23:26:27 +0900

    Add : contents 브랜치

--- client/js/common/bannerSwiper.js
+++ client/js/common/bannerSwiper.js
@@ -1,5 +1,7 @@
 //swiper 기능 구현
-/*
+/* global Swiper */
+
+
 const swiper = new Swiper('#swiper', {
   slidesPerView: 'auto',
   centeredSlides: true,
@@ -23,7 +25,8 @@ const swiper = new Swiper('#swiper', {
   },
   speed: 3.5, // 트랜지션 속도를 0ms로 설정
 });
-*/
+
+
 
 const firstSwiper = new Swiper('#first-swiper', {
   slidesPerView: 6,
@@ -35,55 +38,40 @@ const firstSwiper = new Swiper('#first-swiper', {
   },
 });
 
-const secondSwiper = new Swiper('#second-swiper', {
-  slidesPerView: 4,
-  spaceBetween: 10,
-  // Navigation arrows
-  navigation: {
-    nextEl: '#swiper-button-next-second',
-    prevEl: '#swiper-button-prev-second',
-  },
-});
+
+  const secondSwiper = new Swiper('#second-swiper', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    // Navigation arrows
+    navigation: {
+      nextEl: '#swiper-button-next-second',
+      prevEl: '#swiper-button-prev-second',
+    },
+  });  
 
 
+  const thirdswiper = new Swiper("#third-swiper", {
+    slidesPerView: 12,
+    spaceBetween: 30,
+    navigation: {
+      prevEl: "#swiper-button-prev-third",
+      nextEl: "#swiper-button-next-third",
+    },
+  });
+  
+  const fourthswiper = new Swiper("#fourth-swiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    navigation: {
+      prevEl: "#swiper-button-prev-fourth",
+      nextEl: "#swiper-button-next-fourth",
+    },
+  });
 
 
-
-/*
-const thirdswiper = new Swiper('.thirdSwiper', {
-  slidesPerView: 8,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: '.third-swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next-third",
-    prevEl: ".swiper-button-prev-third",
-  },
-});
-
-
-const fourthswiper = new Swiper('.fourthSwiper', {
-  slidesPerView: 6,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: '.fourth-swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next-fourth",
-    prevEl: ".swiper-button-prev-fourth",
-  },
-});
-
-*/
-/*
 let swiperOnly = new Swiper('.mySwiper', {
   slidesPerView: 6,
   // spaceBetween: 15,
   freeMode: true,
 });
-*/
+
